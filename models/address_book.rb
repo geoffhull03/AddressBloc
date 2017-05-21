0ca0f615686aac0f3c87c790cde53a397729f21e
Geoff Hull <geoffhull03@gmail.com>
2017-05-21 14:58:49 -0700

    Added CSV file and tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -8,12 +9,15 @@ class AddressBook
   end
 
   def remove_entry(name, phone, email)
-    delete = nil
+    delete_entry = nil
+
     @entries.each do |entry|
-      if name ==  entry.name && phone_number == entry.phone_number && entry.email == email
-        delete = entry
+      if name ==  entry.name && phone == entry.phone_number && email == entry.email
+        delete_entry = entry
       end
     end
+
+    @entries.delete(delete_entry)
   end
 
 
@@ -30,4 +34,16 @@ class AddressBook
      # #11
      @entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+
+   end
+
 end
