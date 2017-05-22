3bd839922e6aaf72d37ed60ca7e4df2efdb3dd67
Geoff Hull <geoffhull03@gmail.com>
2017-05-22 12:44:35 -0700

    Added Iterative Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -65,7 +65,17 @@ class AddressBook
          lower = mid + 1
        end
      end
- 
+
      return nil
   end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+  end
+  
 end
